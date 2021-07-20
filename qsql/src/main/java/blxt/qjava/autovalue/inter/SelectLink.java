8a78414103e9d27b513de155ce137fa10827b262
zhangjialei <zhang.jialei@outlook.com>
2021-07-20 18:26:03 +0800

    拆分 SelectLink

---
+++ qsql/src/main/java/blxt/qjava/autovalue/inter/SelectLink.java
@@ -0,0 +1,38 @@
+package blxt.qjava.autovalue.inter;
+
+/**
+ * @author OpenJialei
+ * @date 2021年07月20日 11:48
+ */
+
+import blxt.qjava.autovalue.bean.JoinEnum;
+
+import java.lang.annotation.*;
+
+/**
+ * SelectLink 字段 连表
+ * @author ZhangJieLei
+ */
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface SelectLink {
+
+    /** 连表, A表名 */
+    String joinTableA() default "";
+    /** 连表, B表名 */
+    String joinTableB() default "";
+    /** 连表, B表名,  新名字 */
+    String joinTableBrname() default "";
+    /** 连表, B表字段 */
+    String joinColumB() default "";
+    /** 连表, A表关联键 */
+    String joinKeyA() default "";
+    /** 连表, B表关联键 */
+    String joinKeyB() default "";
+    /** 关联查询
+     * 方式: JOIN, ()LEFT OUTER JOIN,  RIGHT OUT JOIN,  (外连接)FULL OUTER JOIN
+     * */
+    JoinEnum join() default JoinEnum.PG_JOIN;
+
+}
--- qsql/src/main/java/blxt/qjava/autovalue/inter/SqlColumn.java
+++ qsql/src/main/java/blxt/qjava/autovalue/inter/SqlColumn.java
@@ -1,6 +1,7 @@
 package blxt.qjava.autovalue.inter;
 
 import blxt.qjava.autovalue.bean.JoinEnum;
+import lombok.Data;
 
 import java.lang.annotation.*;
 
@@ -36,20 +37,5 @@ public @interface SqlColumn {
     boolean sequence() default false;
     /** 自动更新触发 */
     boolean autoupdate() default false;
-
-    /** 连表, A表名 */
-    String joinTableA() default "";
-    /** 连表, B表名 */
-    String joinTableB() default "";
-    /** 连表, B表字段 */
-    String joinColumB() default "";
-    /** 连表, A表关联键 */
-    String joinKeyA() default "";
-    /** 连表, B表关联键 */
-    String joinKeyB() default "";
-    /** 关联查询
-     * 方式: JOIN, ()LEFT OUTER JOIN,  RIGHT OUT JOIN,  (外连接)FULL OUTER JOIN
-     * */
-    JoinEnum join() default JoinEnum.PG_JOIN;
 
 }
