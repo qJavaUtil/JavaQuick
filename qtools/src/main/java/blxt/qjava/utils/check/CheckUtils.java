c0f087307c5a9dc763fbc49e6fd1433717b00d7f
zhangjialei <zhang.jialei@outlook.com>
2021-10-28 17:41:49 +0800

    更新 pom

--- qtools/src/main/java/blxt/qjava/utils/check/CheckUtils.java
+++ qtools/src/main/java/blxt/qjava/utils/check/CheckUtils.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author MrWang
@@ -15,6 +17,29 @@ import java.util.Set;
  * @Description 自定义校验工具类
  */
 public class CheckUtils {
+    /** 用户名中英文 正则*/
+    static final String NAME_MATCHER_CN = "^[\\w\\-－＿\\.@()（）[０-９]\u4e00-\u9fa5\uFF21-\uFF3A\uFF41-\uFF5A]+$";
+    /** 用户名英文 正则*/
+    static final String NAME_MATCHER_EN = "^[\\w\\-－＿\\.@()（）[０-９]]+$";
+
+    public static void main(String[] args) {
+        CheckUtils.nameCheck("1231asdfag", true);
+        System.out.println("通过0");
+        CheckUtils.nameCheck("1231asdfag", true);
+        System.out.println("通过1");
+        CheckUtils.nameCheck("1231asdfag", true);
+        System.out.println("通过2");
+        CheckUtils.nameCheck("1231Asdfag", false);
+        System.out.println("通过3");
+        CheckUtils.nameCheck("1231Asdfag.", true);
+        System.out.println("通过4");
+        CheckUtils.nameCheck("０1231Asdfag.@", true);
+        System.out.println("通过5");
+        CheckUtils.nameCheck("1231Asdfag.@（）()", true);
+        System.out.println("通过6");
+        CheckUtils.nameCheck("1231Asdfag.@#", true);
+        System.out.println("通过7");
+    }
 
     /**
      * 字符串校验
@@ -91,6 +116,8 @@ public class CheckUtils {
                 throw new DataException(code, message, data);
             }
         }
+
+
     }
 
     public static class FILE{
@@ -127,6 +154,86 @@ public class CheckUtils {
         }
 
     }
+
+    /**
+     * 检查是否满足密码设定
+     *  * 密码不能为空，
+     *  * 密码长度不小于8位数，包含小写字母、大写字母、数字和特殊字符。
+     * @param password     要检查的字符串
+     * @param complexity   需要的复杂度,0 ~ 4, 0 不检查, 4 要求小于8位数，包含小写字母、大写字母、数字和特殊字符。
+     * @return
+     */
+    public static String passwordCheck(String password, int complexity){
+        if (isEmpty(password)) {
+            throw new DataException(20104, "密码不能为空", null);
+        }
+        if(complexity > 0 ){
+            if(password.length() < 8 ){
+                throw new DataException(20104, "密码长度不能小于8", null);
+            }
+            if(password.length() > 511 ){
+                throw new DataException(20104, "密码长度不能长于511", null);
+            }
+            int sum = 0;
+            sum += password.matches(".*[a-z]{1,}.*") ? 1 : 0;
+            sum += password.matches(".*[A-Z]{1,}.*") ? 1 : 0;
+            sum += password.matches(".*\\d{1,}.*") ? 1 : 0;
+            sum += password.matches(".*[~!@#$%^&*\\.?]{1,}.*") ? 1 : 0;
+
+            if(sum < complexity){
+                throw new DataException(20104,
+                    String.format("需要包含小写字母、大写字母、数字和特殊字符(英文符号)中的%d个组合", complexity),
+                    password);
+            }
+        }
+        return password;
+    }
+
+    /**
+     * 检查是否满足 用户名称检查
+     *  * 用户名称不能为空，
+     *  * 用户名称小于2位数，包含小写字母、大写字母、数字
+     *  * 不包含和特殊字符
+     * @param name         要检查的字符串
+     * @param cn           是否支持中文
+     * @return
+     */
+    public static String nameCheck(String name, boolean cn){
+        if (isEmpty(name)) {
+            throw new DataException(20105, "名称不能为空", null);
+        }
+        if(name.length() < 2 ){
+            throw new DataException(20105, "名称长度不能小于2", name);
+        }
+        if(name.length() > 63 ){
+            throw new DataException(20105, "名称长度不能长于63", null);
+        }
+
+        // 正则检查
+        Pattern pattern = null;
+        if(cn){
+            pattern = Pattern.compile(NAME_MATCHER_CN);
+        }
+        else{
+            pattern = Pattern.compile(NAME_MATCHER_EN);
+        }
+
+        Matcher matcher = pattern.matcher(name);
+        if(!matcher.matches()){
+            if(cn){
+                throw new DataException(20105, "名称只能用中英文和数字", name);
+            }
+            else{
+                throw new DataException(20105, "名称只能用英文和数字", name);
+            }
+        }
+
+        return name;
+    }
+
+
+
+
 
 
     /**
