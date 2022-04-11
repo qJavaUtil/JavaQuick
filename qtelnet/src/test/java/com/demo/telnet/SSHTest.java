43e6c991d461065fc1a5e9feb711da688b2d7af0
zhangjialei <zhang.jialei@outlook.com>
2022-04-11 09:11:39 +0800

    更新demo

--- qtelnet/src/test/java/com/demo/telnet/SSHTest.java
+++ qtelnet/src/test/java/com/demo/telnet/SSHTest.java
@@ -1,11 +1,10 @@
 package com.demo.telnet;
 
 import blxt.qjava.qssh.QSSH;
-import ch.ethz.ssh2.Connection;
-import ch.ethz.ssh2.Session;
-import ch.ethz.ssh2.StreamGobbler;
+import blxt.qjava.qtelnet.QTelnetClient;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author OpenJialei
@@ -13,113 +12,60 @@ import java.io.*;
  */
 public class SSHTest {
 
-    private static String DEFAULTCHART = "UTF-8";
-
-    public static Connection login(String ip, String username, String password) {
-        boolean flag = false;
-        Connection connection = null;
-        try {
-            connection = new Connection(ip);
-            connection.connect();// 连接
-            flag = connection.authenticateWithPassword(username, password);// 认证
-            if (flag) {
-                session = connection.openSession();// 打开一个会话
-                System.out.println("================登录成功==================");
-                return connection;
-            }
-        } catch (IOException e) {
-            System.out.println("=========登录失败=========" + e);
-            connection.close();
-        }
-        return connection;
-    }
-
-    /**
-     * 远程执行shll脚本或者命令
-     *
-     * @param cmd
-     *            即将执行的命令
-     * @return 命令执行完后返回的结果值
-     */
-    static Session session;
-    public static String execmd(Connection connection, String cmd) {
-        String result = "";
-        try{
-            if (connection != null) {
-                if(session == null){
-                    session = connection.openSession();// 打开一个会话
-                }
-                session.execCommand(cmd);// 执行命令
-                result = processStdout(session.getStdout(), DEFAULTCHART);
-                System.out.println(result);
-                // 如果为得到标准输出为空，说明脚本执行出错了
-                if (result == null || result.isEmpty()) {
-                    System.out.println("得到标准输出为空,链接conn:" + connection + ",执行的命令：" + cmd);
-                    result = processStdout(session.getStderr(), DEFAULTCHART);
-                } else {
-                    System.out.println("执行命令成功,链接conn:" + connection + ",执行的命令：" + cmd);
-                }
-               // connection.close();
-               // session.close();
-            }
-        } catch (IOException e) {
-            System.out.println("执行命令失败,链接conn:" + connection + ",执行的命令：" + cmd + "  " + e);
-            e.printStackTrace();
-        }
-        return result;
-
-    }
-
-    /**
-     * 解析脚本执行返回的结果集
-     *
-     * @param in
-     *            输入流对象
-     * @param charset
-     *            编码
-     * @return 以纯文本的格式返回
-     */
-    private static String processStdout(InputStream in, String charset) {
-        InputStream stdout = new StreamGobbler(in);
-        StringBuffer buffer = new StringBuffer();
-        ;
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(stdout, charset));
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                buffer.append(line + "\n");
-                System.out.println(line);
-            }
-            br.close();
-        } catch (UnsupportedEncodingException e) {
-            System.out.println("解析脚本出错：" + e.getMessage());
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("解析脚本出错：" + e.getMessage());
-            e.printStackTrace();
-        }
-        return buffer.toString();
-    }
-
     public static void main(String[] args) throws IOException {
-        QSSH qssh = new QSSH();
-        qssh.setHost("192.168.10.198");
-        qssh.setUserName("root");
-        qssh.setPassword("nm,.qwer");
-
-//        qssh.connect();
-//        System.out.println("ls: " + qssh.exeCommand("ls"));
-//        System.out.println("cd: " +qssh.exeCommand("cd /"));
-//        System.out.println("ls: " +qssh.exeCommand("ls"));
-
         String ip = "192.168.10.198";//change your ip
         String username = "root";//user
         String password = "nm,.qwer";//your server password
 
-        Connection connection = login(ip, username, password);
+        QSSH javaSsh = new QSSH()
+            .setTag("测试")
+            .setHost(ip)
+            .setUserName(username)
+            .setPassword(password)
+            .build();
 
-        session.execCommand("cd /");
-        System.out.println("执行结果 "+ execmd(connection, "ls"));
+        // 回显
+        javaSsh.setOnTelnetClientListener(new QTelnetClient.OnTelnetClientListener() {
+            @Override
+            public void onConnect(String tag, boolean isconnect) {
+                System.out.println("连接" + isconnect);
+            }
 
+            @Override
+            public void onDisConnect(String tag, boolean isconnect) {
+                System.out.println("断开" + isconnect);
+            }
+
+            @Override
+            public void onLogin(String tag, boolean isLogin) {
+
+            }
+
+            @Override
+            public void onGetDate(String tag, byte[] data) {
+                String msg =  new String(data, StandardCharsets.UTF_8);
+                System.out.println(msg);
+            }
+
+            @Override
+            public void onReceiver(String tag, String msg) {
+            }
+        });
+
+        if(!javaSsh.connect()){
+            System.out.println("连接失败");
+            return;
+        }
+
+        // 连续输入
+        javaSsh.sendPush("ls");
+        javaSsh.sendPush("cd /");
+        javaSsh.sendPush("ls");
+        // 独立执行
+        System.out.println( "<<" + javaSsh.sendStandalone("pwd\n"));
+        javaSsh.sendPush("pwd");
+
+        // 如果太早关闭, 回显会来不及接收
+        // javaSsh.close();
     }
 }
